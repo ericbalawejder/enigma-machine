c2f9b70755dcb09a0dee264c72617e25d4e7ecbd
Eric Balawejder <eric.balawejder@gmail.com>
2020-12-30 14:01:51 -0500

    Added more tests for EnigmaMachine

--- src/test/java/com/machine/enigma/EnigmaMachineTest.java
+++ src/test/java/com/machine/enigma/EnigmaMachineTest.java
@@ -3,6 +3,8 @@ package com.machine.enigma;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EnigmaMachineTest {
@@ -20,7 +22,7 @@ class EnigmaMachineTest {
     }
 
     @Test
-    void encodeLine() {
+    void testEncodeLine() {
         String expected = "WGJOUNHUPLJRETLGWMNRPIIXUDM";
 
         enigmaMachine.setRotors(5, 9, 14);
@@ -30,13 +32,81 @@ class EnigmaMachineTest {
     }
 
     @Test
-    void encodeLineIsSymmetrical() {
+    void testEncodeLineIsSymmetrical() {
         String expected = "AAAAAAAAAAAAAAAAAAAAAAAAAAA";
 
         enigmaMachine.setRotors(5, 9, 14);
         String actual = enigmaMachine.encodeLine("WGJOUNHUPLJRETLGWMNRPIIXUDM");
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSetRotors() {
+        enigmaMachine.setRotors(17, 4, 9);
+        assertEquals(enigmaMachine.getR1().getRotorPosition(), 17);
+        assertEquals(enigmaMachine.getR2().getRotorPosition(), 4);
+        assertEquals(enigmaMachine.getR3().getRotorPosition(), 9);
+    }
+
+    @Test
+    void testIncrementRotorsR1() {
+        enigmaMachine.setRotors(0, 0, 0);
+        enigmaMachine.incrementRotors();
+        assertEquals(enigmaMachine.getR1().getRotorPosition(), 1);
+        assertEquals(enigmaMachine.getR2().getRotorPosition(), 0);
+        assertEquals(enigmaMachine.getR3().getRotorPosition(), 0);
+    }
+
+    @Test
+    void testIncrementRotorsR2() {
+        enigmaMachine.setRotors(25, 0, 0);
+        enigmaMachine.incrementRotors();
+        assertEquals(enigmaMachine.getR1().getRotorPosition(), 0);
+        assertEquals(enigmaMachine.getR2().getRotorPosition(), 1);
+        assertEquals(enigmaMachine.getR3().getRotorPosition(), 0);
+    }
+
+    @Test
+    void testIncrementRotorsR3() {
+        enigmaMachine.setRotors(25, 25, 0);
+        enigmaMachine.incrementRotors();
+        assertEquals(enigmaMachine.getR1().getRotorPosition(), 0);
+        assertEquals(enigmaMachine.getR2().getRotorPosition(), 0);
+        assertEquals(enigmaMachine.getR3().getRotorPosition(), 1);
+    }
+
+    @Test
+    void testIncrementRotorsR1R2R3() {
+        enigmaMachine.setRotors(25, 25, 25);
+        enigmaMachine.incrementRotors();
+        assertEquals(enigmaMachine.getR1().getRotorPosition(), 0);
+        assertEquals(enigmaMachine.getR2().getRotorPosition(), 0);
+        assertEquals(enigmaMachine.getR3().getRotorPosition(), 0);
+    }
+
+    @Test
+    void testIncrementRotors5Times() {
+        enigmaMachine.setRotors(23, 19, 13);
+
+        IntStream.range(0, 5)
+                .forEach(e -> enigmaMachine.incrementRotors());
+
+        assertEquals(enigmaMachine.getR1().getRotorPosition(), 2);
+        assertEquals(enigmaMachine.getR2().getRotorPosition(), 20);
+        assertEquals(enigmaMachine.getR3().getRotorPosition(), 13);
+    }
+
+    @Test
+    void testIncrementRotors78Times() {
+        enigmaMachine.setRotors(2, 21, 13);
+
+        IntStream.range(0, 78)
+                .forEach(e -> enigmaMachine.incrementRotors());
+
+        assertEquals(enigmaMachine.getR1().getRotorPosition(), 2);
+        assertEquals(enigmaMachine.getR2().getRotorPosition(), 24);
+        assertEquals(enigmaMachine.getR3().getRotorPosition(), 13);
     }
 
 }
