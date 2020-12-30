adaf54a8987ee164931bc91c180dcf841230ffac
Eric Balawejder <eric.balawejder@gmail.com>
2020-12-30 01:16:20 -0500

    Refactored Reflector

--- src/main/java/com/machine/enigma/Reflector.java
+++ src/main/java/com/machine/enigma/Reflector.java
@@ -1,18 +1,19 @@
 package com.machine.enigma;
 
+import java.util.stream.IntStream;
+
 class Reflector {
 
-    static final int MAX = Rotor.MAX;
-    static final int A = Rotor.A;
-    private static final char[] reflector = new char[MAX];
+    private static final int MAX = Rotor.MAX;
+    private static final int A = Rotor.A;
+    private final char[] reflector = new char[MAX];
 
     Reflector(String sequence) {
-        for (int i = 0; i < MAX; i++) {
-            reflector[i] = sequence.charAt(i);
-        }
+        IntStream.range(0, MAX)
+                .forEach(i -> reflector[i] = sequence.charAt(i));
     }
 
-    char reflect(char c) {
+    char characterIndexValue(char c) {
         return reflector[c - A];
     }
 
