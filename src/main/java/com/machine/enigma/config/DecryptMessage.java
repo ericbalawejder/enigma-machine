c23c177c9893959d009268512097e844255d7250
Eric Balawejder <eric.balawejder@gmail.com>
2021-01-13 18:29:21 -0500

    Encrypted and unencrypted file from same Enigma machine

--- src/main/java/com/machine/enigma/config/DecryptMessage.java
+++ src/main/java/com/machine/enigma/config/DecryptMessage.java
@@ -6,11 +6,9 @@ import com.machine.enigma.cipher.Rotor;
 import com.machine.enigma.encryption.Encrypt;
 import com.machine.enigma.io.ReadFile;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 
-public class EncryptMessage {
+public class DecryptMessage {
 
     public static void main(String[] args) throws IOException {
 
@@ -20,12 +18,11 @@ public class EncryptMessage {
         Reflector rf = new Reflector("QYHOGNECVPUZTFDJAXWMKISRBL");
         EnigmaMachine enigmaMachine = new EnigmaMachine(r1, r2, r3, rf);
 
-        final String file = ReadFile.read("src/main/resources/unencrypted.txt");
+        final String file = ReadFile.read("src/main/resources/encrypted.txt");
 
         final Encrypt encrypt = new Encrypt();
         final String encryptedFile = encrypt.encryptFile(enigmaMachine, file);
 
-        System.setOut(new PrintStream(new FileOutputStream("src/main/resources/encrypted.txt")));
         System.out.println(encryptedFile);
     }
 
--- src/main/java/com/machine/enigma/encryption/Decrypt.java
+++ src/main/java/com/machine/enigma/encryption/Decrypt.java
@@ -11,36 +11,20 @@ import java.util.List;
 
 public class Decrypt {
 
-    public static void main(String[] args) throws IOException {
-
-        final String fileName = "src/main/resources/encrypt-file.txt";
-
-        final Decrypt user = new Decrypt();
-
-        //final List<Integer> rotorSettings = user.findRotorSettings(enigmaMachine, fileName, 10);
-
-        //final String unencryptedFile = user.decryptFile(enigmaMachine, rotorSettings, fileName);
-
-        //System.setOut(new PrintStream(new FileOutputStream("src/main/resources/test.txt")));
-        //System.out.println(unencryptedFile);
-    }
-
-    String decryptFile(EnigmaMachine enigmaMachine, List<Integer> rotorSettings, String fileName)
+    public String decryptFile(EnigmaMachine enigmaMachine, List<Integer> rotorSettings, String fileName)
             throws IOException {
         if (rotorSettings.isEmpty()) {
             throw new RuntimeException("No rotor settings satisfy language constraints");
         }
         enigmaMachine.setRotors(rotorSettings.get(0), rotorSettings.get(1), rotorSettings.get(2));
-        final ReadFile file = new ReadFile(fileName);
-        return enigmaMachine.encodeLine(file.read());
+        return enigmaMachine.encodeLine(ReadFile.read(fileName));
     }
 
-    List<Integer> findRotorSettings(EnigmaMachine enigmaMachine, String fileName)
+    public List<Integer> findRotorSettings(EnigmaMachine enigmaMachine, String fileName)
             throws IOException {
 
-        final ReadFile file = new ReadFile(fileName);
         final English english = new English();
-        final String encryptedFile = file.read();
+        final String encryptedFile = ReadFile.read(fileName);
 
         for (int i = 0; i < Rotor.MAX; i++) {
             for (int j = 0; j < Rotor.MAX; j++) {
@@ -60,12 +44,11 @@ public class Decrypt {
         return new ArrayList<>();
     }
 
-    List<Integer> findRotorSettings(EnigmaMachine enigmaMachine, String fileName, int numberOfLinesToSample)
+    public List<Integer> findRotorSettings(EnigmaMachine enigmaMachine, String fileName, int numberOfLinesToSample)
             throws IOException {
 
-        final ReadFile file = new ReadFile(fileName);
         final English english = new English();
-        final String encryptedFile = file.read(numberOfLinesToSample);
+        final String encryptedFile = ReadFile.read(fileName, numberOfLinesToSample);
 
         for (int i = 0; i < Rotor.MAX; i++) {
             for (int j = 0; j < Rotor.MAX; j++) {
